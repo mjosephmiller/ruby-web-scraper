0b32f9b18e8966c6739e0f4c4da9aa433d90a394
Michael Miller <mjosephmiller.uk@gmail.com>
2018-01-17 01:34:07 +0100

    use mechanize to navigate advanced search

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,41 +1,44 @@
-require 'HTTParty'
-require 'Nokogiri'
 require 'csv'
+require 'mechanize'
+require 'pp'
 
 class Scraper
     attr_accessor :parse_page
     attr_accessor :doc
+    attr_accessor :agent
 
     def initialize
-        @doc = HTTParty.get('http://www.wegottickets.com/searchresults/all')
+        @agent = Mechanize.new
+        @doc = @agent.get('http://www.wegottickets.com/searchresults/adv')
     end
 
-    def scrape_current_page
-        @parse_page = Nokogiri::HTML(@doc)
+    def search
+        form = @doc.forms[1]
+        form.field_with(name: 'adv_genre').options[8].click
+        form.submit
     end
 
     def events
         events = []
         another_page = true
         page_num = 1
-        while another_page == true && page_num <= 3
-            scrape_current_page
-            next_page_button = @parse_page.css("#paginate").css('.block-group.advance-filled.section-margins.padded.text-center').css('a').text.include? 'next'
-            @parse_page.css(".content.block-group.chatterbox-margin").each do |event|
+        while another_page == true && page_num >= 1
+            next_page_button = @doc.at('#paginate').at('.block-group.advance-filled.section-margins.padded.text-center').at('.pagination_link_text.nextlink').children[1].text == 'next' rescue false
+            @doc.search(".content.block-group.chatterbox-margin").each do |event|
                 event_hash = {}
-                event_hash[:event_name] = event.css('a.event_link').text
-                event_hash[:artist_name] = event.css('.block.diptych.chatterbox-margin').css('.venue-details').css('h4 i').text unless event.css('.block.diptych.chatterbox-margin').css('.venue-details').css('h4 i').text.empty?
-                event_hash[:venue_name] = event.css('.block.diptych.chatterbox-margin').css('.venue-details').css('h4').first.text.split(': ').last
-                event_hash[:city] = event.css('.block.diptych.chatterbox-margin').css('.venue-details').css('h4').first.text.split(': ').first
-                event_hash[:date] = event.css('.block.diptych.chatterbox-margin').css('.venue-details').css('h4')[1].text
-                event_hash[:price] = event.css('.block.diptych.text-right').css('.searchResultsPrice').css('strong').text
+                event_hash[:event_name] = event.at('a.event_link').text
+                event_hash[:artist_name] = event.at('.block.diptych.chatterbox-margin').at('.venue-details').at('h4 i').text unless event.at('.block.diptych.chatterbox-margin').at('.venue-details').at('h4 i').nil?
+                event_hash[:venue_name] = event.at('.block.diptych.chatterbox-margin').at('.venue-details').at('h4').text.split(': ').last
+                event_hash[:city] = event.at('.block.diptych.chatterbox-margin').at('.venue-details').at('h4').text.split(': ').first
+                event_hash[:date] = event.at('.block.diptych.chatterbox-margin').at('.venue-details').search('h4')[1].text
+                event_hash[:price] = event.at('.block.diptych.text-right').at('.searchResultsPrice').at('strong').text rescue '0.00'
                 events << event_hash
             end
             page_num += 1
             if next_page_button == false
                 another_page = false
             else
-                @doc = HTTParty.get("http://www.wegottickets.com/searchresults/page/#{page_num}/all#paginate")
+                @doc = @agent.get("http://www.wegottickets.com/searchresults/page/#{page_num}/adv#paginate")
             end
         end
         events
